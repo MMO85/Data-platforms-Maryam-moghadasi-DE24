4a04cb32bdcf0e6d69f721158175c5c0b69a1650
Marmog85 <moghadasi.64@gmail.com>
2025-01-29 10:54:12 +0100

    read in csv file

--- code_alongs/05_dockerfile/main.py
+++ code_alongs/05_dockerfile/main.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+import pandas as pd
 
 Data_path = Path(__file__).parent/ "data"
-
 print(Data_path / "Prog_book.csv")
+
+df = pd.read_csv(Data_path / "Prog_book.csv")
+print(df.head())
